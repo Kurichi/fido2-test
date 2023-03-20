cf11517c0af75bd0978affce091415b9bc49e7af
Kurichi <yuuyapp080@gmail.com>
2023-03-20 12:43:28 +0900

    [add] CredentialList

--- backend/app/model/user.go
+++ backend/app/model/user.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"github.com/go-webauthn/webauthn/protocol"
 	"github.com/go-webauthn/webauthn/webauthn"
 )
 
@@ -29,4 +30,17 @@ func (u *User) WebAuthnIcon() string {
 
 func (u *User) WebAuthnCredentials() []webauthn.Credential {
 	return u.Credentials
+}
+
+func (u *User) CredentialExcludeList() []protocol.CredentialDescriptor {
+	credentialExcludeList := []protocol.CredentialDescriptor{}
+	for _, cred := range u.Credentials {
+		descriptor := protocol.CredentialDescriptor{
+			Type:         protocol.PublicKeyCredentialType,
+			CredentialID: cred.ID,
+		}
+		credentialExcludeList = append(credentialExcludeList, descriptor)
+	}
+
+	return credentialExcludeList
 }
